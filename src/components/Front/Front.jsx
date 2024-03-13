0eafe3dba8c90e05db91549329bb926c7910a16b
mariambuzaladze <mariambuzaladze10@example.come>
2024-03-13 20:10:27 +0400

    add and style card front

--- src/components/Front/Front.jsx
+++ src/components/Front/Front.jsx
@@ -1,6 +1,21 @@
 import "./front.css";
 import CardFront from "/images/bg-card-front.png";
+import Circle from "/images/card-logo.svg";
 
 export default function Front() {
-  return <img className="card-front" src={CardFront} alt="card front" />;
+  return (
+    <>
+      <img className="card-front" src={CardFront} alt="card front" />
+      <img className="card-logo" src={Circle} alt="card logo" />
+
+      <div className="front-text">
+        <p className="card-number">0000 0000 0000 0000</p>
+
+        <div className="name-date">
+          <p className="card-name">JANE APPLESEED</p>
+          <p className="card-date">00/00</p>
+        </div>
+      </div>
+    </>
+  );
 }
